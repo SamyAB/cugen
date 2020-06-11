ba52e5a47ffab35c2f898dcdeccd1bb3acaa4fbb
SamyAB <aitb.samy@gmail.com>
2020-06-11 16:56:29 +0200

    FEAT: add an optimizer to loop over the generation to find the best individual

--- cugen/sat_problem_modelling/optimizer.py
+++ cugen/sat_problem_modelling/optimizer.py
@@ -1,5 +1,10 @@
 import cupy
 
+from cugen.sat_problem_modelling.crossover import population_binary_crossover
+from cugen.sat_problem_modelling.evaluation import evaluate_population
+from cugen.sat_problem_modelling.mutation import population_mutation
+from cugen.sat_problem_modelling.selection import select_individuals
+
 
 def generate_random_first_generation(population_size, number_of_genes_in_population) -> cupy.ndarray:
     """
@@ -14,10 +19,34 @@ def generate_random_first_generation(population_size, number_of_genes_in_populat
 
 def optimize(formula: cupy.ndarray, maximum_number_of_generations: int, population_size: int) -> cupy.ndarray:
     """
+    Finds the best solution to the input formula within the number of generation given as input.
 
-    :param formula:
-    :param maximum_number_of_generations:
-    :param population_size:
-    :return:
+    The optimizer stops, either if it finds the perfect individual, or if the number of generation equals the maximum
+    number of generations
+
+    :param formula: The formula to solve
+    :param maximum_number_of_generations: The maximum of number of generations before the stopping of the optimizer
+    :param population_size: The number of individuals in each generation
+    :return: The best individual found
     """
-    pass
+    population = generate_random_first_generation(population_size, formula.shape[1])
+    population_fitness = evaluate_population(population, formula)
+    best_fitness = cupy.max(population_fitness)
+    best_individual = population_fitness[cupy.argmax(best_fitness)]
+
+    if best_fitness == 1.:
+        return best_individual
+
+    for _ in range(maximum_number_of_generations):
+        breading_population = select_individuals(population, population_fitness)
+        population = population_mutation(population_binary_crossover(breading_population, population_size))
+        population_fitness = evaluate_population(population, formula)
+
+        if cupy.max(population_fitness) > best_fitness:
+            best_fitness = cupy.max(population_fitness)
+            best_individual = population[cupy.argmax(population_fitness)]
+
+            if best_fitness == 1.:
+                break
+
+    return best_individual
--- tests/test_sat_problem_modelling/test_optimizer.py
+++ tests/test_sat_problem_modelling/test_optimizer.py
@@ -1,13 +1,92 @@
-from cugen.sat_problem_modelling.optimizer import generate_random_first_generation
+from unittest.mock import patch, call
+
+import cupy
+
+from cugen.sat_problem_modelling.optimizer import generate_random_first_generation, optimize
+
+TESTED_MODULE = 'cugen.sat_problem_modelling.optimizer'
 
 
-def test_optimize_should_call_the_right_functions_in_the_right_order():
+@patch(f'{TESTED_MODULE}.generate_random_first_generation')
+@patch(f'{TESTED_MODULE}.evaluate_population')
+@patch(f'{TESTED_MODULE}.select_individuals')
+@patch(f'{TESTED_MODULE}.population_binary_crossover')
+@patch(f'{TESTED_MODULE}.population_mutation')
+@patch(f'{TESTED_MODULE}.cupy.max')
+@patch(f'{TESTED_MODULE}.cupy.argmax')
+def test_optimize_should_call_the_right_functions_in_the_right_order(mock_argmax, mock_max, mock_mutation,
+                                                                     mock_crossover, mock_selection,
+                                                                     mock_evaluate_population,
+                                                                     mock_generate_first_generation):
     # Given
+    formula = cupy.array([
+        [1, cupy.nan, cupy.nan],
+        [0, cupy.nan, cupy.nan]
+    ])
+    number_of_iterations = 10
+    population_size = 2
+
+    mock_generate_first_generation.return_value = "Population"
+    mock_evaluate_population.return_value = "Population evaluation array"
+    mock_selection.return_value = "Selected population"
+    mock_crossover.return_value = "Crossover population"
+    mock_mutation.return_value = "Population"
+    mock_max.return_value = 15
+    mock_argmax.return_value = 0
+
+    expected_evaluate_population_calls = [call("Population", formula)] * (number_of_iterations + 1)
+    expected_selection_calls = [call("Population", "Population evaluation array")] * number_of_iterations
+    expected_crossover_calls = [call("Selected population", population_size)] * number_of_iterations
+    expected_mutation_calls = [call("Crossover population")]
 
     # When
+    _ = optimize(formula, number_of_iterations, population_size)
 
     # Then
-    pass
+    mock_generate_first_generation.assert_called_once_with(population_size, formula.shape[1])
+    mock_evaluate_population.has_calls(expected_evaluate_population_calls)
+    mock_selection.assert_has_calls(expected_selection_calls)
+    mock_crossover.assert_has_calls(expected_crossover_calls)
+    mock_mutation.assert_has_calls(expected_mutation_calls)
+
+
+@patch(f'{TESTED_MODULE}.evaluate_population')
+@patch(f'{TESTED_MODULE}.select_individuals')
+@patch(f'{TESTED_MODULE}.population_mutation')
+@patch(f'{TESTED_MODULE}.population_binary_crossover')
+def test_optimizer_stops_when_the_perfect_individual_has_been_found_in_a_generation(mock_crossover,
+                                                                                    mock_mutation,
+                                                                                    mock_selection,
+                                                                                    mock_evaluation):
+    # Given
+    formula = cupy.array([
+        [0, 0, 1],
+        [cupy.nan, cupy.nan, 0]
+    ])
+    number_of_generations = 15
+    population_size = 4
+
+    mock_evaluation.side_effect = [cupy.array([0.5, 0.2, 0.3, 0.7]), cupy.array([1.0, 0.2, 0.3, 0.4])]
+
+    # When
+    _ = optimize(formula, number_of_generations, population_size)
+
+    # Then
+    mock_selection.assert_called_once()
+
+
+@patch(f'{TESTED_MODULE}.evaluate_population', return_value=cupy.array([1.0, 0.2, 0.3]))
+def test_optimize_does_not_start_optimization_if_there_is_a_perfect_individual_in_the_initial_generation(mock_evaluate):
+    # Given
+    formula = cupy.array([[1., 0., 1.]])
+    number_of_generations = 10
+    population_size = 3
+
+    # When
+    _ = optimize(formula, number_of_generations, population_size)
+
+    # Then
+    mock_evaluate.assert_called_once()
 
 
 def test_generate_random_first_generation_returns_population_with_the_right_shape():
