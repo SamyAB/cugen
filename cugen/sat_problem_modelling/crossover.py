fe5b0186fe787e8bb4e3150cc1e20c6de64545b6
SamyAB <aitb.samy@gmail.com>
2020-06-11 11:16:42 +0200

    FEAT: create a population binary crossover function

--- cugen/sat_problem_modelling/crossover.py
+++ cugen/sat_problem_modelling/crossover.py
@@ -11,3 +11,33 @@ def uniform_binary_crossover(first_parent_individual: cupy.ndarray,
     :return: A new individual from the mixing of the two input individuals
     """
     return cupy.where(cupy.random.uniform() <= 0.5, first_parent_individual, second_parent_individual)
+
+
+def population_binary_crossover(population: cupy.ndarray, next_generation_size: int) -> cupy.ndarray:
+    """
+    Creates a new generation of individuals, where each individual is a child of two individuals from the input
+    population. The parents are chosen randomly, and the crossover is uniform, and the new generation has a fixed size
+
+    :param population: The parents
+    :param next_generation_size: The number of individuals to generate in the new population
+    :return: The new population with next_generation_size individuals
+    """
+    number_of_parents = population.shape[0]
+    last_mating_individual_index = number_of_parents if number_of_parents % 2 == 0 else number_of_parents - 2
+
+    minimum_number_of_matings = next_generation_size // (number_of_parents // 2) + 1
+
+    all_mating_seasons_children = []
+    for _ in range(minimum_number_of_matings):
+        shuffled_population = cupy.random.permutation(population)
+        pair_parents = shuffled_population[::2]
+        odd_parents = shuffled_population[1: last_mating_individual_index:2]
+
+        mating_season_children = cupy.where(cupy.random.uniform() <= 0.5, pair_parents, odd_parents)
+
+        all_mating_seasons_children.append(mating_season_children)
+
+    new_generation = cupy.concatenate(all_mating_seasons_children)[:next_generation_size]
+
+    return new_generation
+
--- tests/test_sat_problem_modelling/test_crossover.py
+++ tests/test_sat_problem_modelling/test_crossover.py
@@ -1,8 +1,9 @@
 from unittest.mock import patch
 
 import cupy
+import pytest
 
-from cugen.sat_problem_modelling.crossover import uniform_binary_crossover
+from cugen.sat_problem_modelling.crossover import uniform_binary_crossover, population_binary_crossover
 
 TESTED_MODULE = 'cugen.sat_problem_modelling.crossover'
 
@@ -22,3 +23,42 @@ def test_uniform_binary_crossover_generates_a_new_individual_combining_the_two_p
 
     # Then
     cupy.testing.assert_array_equal(child_individual, expected_child_individual)
+
+
+@pytest.mark.parametrize("next_generation_size", [4, 5, 6, 7])
+def test_crossover_population_yields_a_population_with_input_size_when_the_number_of_parents_is_even(
+        next_generation_size
+):
+    # Given
+    population = cupy.array([
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 0],
+        [0, 0, 1, 1]
+    ])
+
+    # When
+    new_generation = population_binary_crossover(population, next_generation_size)
+
+    # Then
+    assert new_generation.shape[0] == next_generation_size
+
+
+@pytest.mark.parametrize("next_generation_size", [4, 5, 6, 7])
+def test_crossover_population_yields_a_population_with_input_size_when_the_number_of_parents_is_odd(
+        next_generation_size
+):
+    # Given
+    population = cupy.array([
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 0],
+        [0, 0, 1, 1],
+        [1, 1, 1, 1],
+    ])
+
+    # When
+    new_generation = population_binary_crossover(population, next_generation_size)
+
+    # Then
+    assert new_generation.shape[0] == next_generation_size
