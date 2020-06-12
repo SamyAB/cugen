4c41dc453a7c06c6ce5ff31f781a9330481179de
SamyAB <aitb.samy@gmail.com>
2020-06-12 16:25:25 +0200

    REFACTO: extract the mutation probability as a parameter of the mutation functions

--- cugen/sat_problem_modelling/mutation.py
+++ cugen/sat_problem_modelling/mutation.py
@@ -1,19 +1,18 @@
 import cupy
 
-MUTATION_PROBABILITY = .01
 
-
-def individual_mutation(individual: cupy.ndarray) -> cupy.ndarray:
+def individual_mutation(individual: cupy.ndarray, mutation_probability: float) -> cupy.ndarray:
     """
     Applies a mutation on the input individual, if a random value (from a uniform distribution) is above the mutation
     probability threshold. The literal to be mutated is also selected randomly in the cas of a mutation.
 
     If the threshold is not met, the function returns the input individual
 
+    :param mutation_probability: Between 0 and 1. The probability that has an individual to mutate
     :param individual: The individual to mutate
     :return: Either the input individual if the threshold is not met, or the mutated individual otherwise
     """
-    if cupy.random.uniform() <= MUTATION_PROBABILITY:
+    if cupy.random.uniform() <= mutation_probability:
         mutated_individual = individual.copy()
         element_to_mutate = cupy.random.randint(low=0, high=mutated_individual.shape[0])
         mutated_individual[element_to_mutate] = 1 - mutated_individual[element_to_mutate]
@@ -22,17 +21,18 @@ def individual_mutation(individual: cupy.ndarray) -> cupy.ndarray:
     return individual
 
 
-def population_mutation(population: cupy.ndarray) -> cupy.ndarray:
+def population_mutation(population: cupy.ndarray, mutation_probability: float) -> cupy.ndarray:
     """
     Runs the individual mutation for every individual in a given population
 
     Note : This function might be a bottleneck as it uses a python for loop, if anyone has an idea that would make
     go faster, please share it
 
+    :param mutation_probability: Between 0 and 1. The probability that has an individual to mutate
     :param population: The set of individuals to mutate
     :return: The input population with individual randomly mutated
     """
     for individual_index, individual in enumerate(population):
-        population[individual_index] = individual_mutation(individual)
+        population[individual_index] = individual_mutation(individual, mutation_probability)
 
     return population
--- tests/test_sat_problem_modelling/test_mutation.py
+++ tests/test_sat_problem_modelling/test_mutation.py
@@ -12,11 +12,12 @@ TESTED_MODULE = 'cugen.sat_problem_modelling.mutation'
 def test_individual_mutation_should_change_change_one_literal_value_in_the_individual(mock_uniform, mock_randint):
     # Given
     individual = cupy.array([0, 0, 0, 0])
+    mutation_probability = .25
 
     expected_mutated_individual = cupy.array([1, 0, 0, 0])
 
     # When
-    mutated_individual = individual_mutation(individual)
+    mutated_individual = individual_mutation(individual, mutation_probability)
 
     # Then
     cupy.testing.assert_array_equal(mutated_individual, expected_mutated_individual)
@@ -26,9 +27,10 @@ def test_individual_mutation_should_change_change_one_literal_value_in_the_indiv
 def test_individual_mutation_returns_the_input_individual_if_random_value_is_bellow_mutation_probability(_):
     # Given
     input_individual = cupy.array([1, 2, 3, 4])
+    mutation_probability = .25
 
     # When
-    mutated_individual = individual_mutation(input_individual)
+    mutated_individual = individual_mutation(input_individual, mutation_probability)
 
     # Then
     assert mutated_individual is input_individual
@@ -45,6 +47,7 @@ def test_population_mutation_should_apply_the_mutation_on_all_individuals_of_the
         [0, 1, 0, 1],
         [1, 0, 1, 0],
     ])
+    mutation_probability = .25
 
     mock_uniform.side_effect = [cupy.array(0.001), cupy.array(1), cupy.array(1), cupy.array(1), cupy.array(0.001)]
 
@@ -57,7 +60,7 @@ def test_population_mutation_should_apply_the_mutation_on_all_individuals_of_the
     ])
 
     # When
-    mutated_population = population_mutation(population)
+    mutated_population = population_mutation(population, mutation_probability)
 
     # Then
     cupy.testing.assert_array_equal(mutated_population, expected_mutated_population)
